e1732c4a132fa2b3753887d065f131ebf4d60d72
tofatoff <mail.mstofa@gmail.com>
2022-04-02 22:47:12 +0700

    make hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
-    content: ["index.html"],
-    theme: {
-        extend: {},
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
     },
-    plugins: [],
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
 };
