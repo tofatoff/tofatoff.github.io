8253a4b5723a073823da98053915dcc0cb73e431
tofatoff <mail.mstofa@gmail.com>
2023-10-13 13:44:31 +0700

    change padding x and set container

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,20 +3,20 @@ import TFLogo from "../../assets/tf logo.svg";
 
 const Navbar = () => {
 	return (
-		<div className="p-2 border border-cyan-800 flex justify-between items-center fixed top-0 w-full bg-white">
-			<div className="flex items-center">
-				<img src={TFLogo} width={50} height={50} />
-				<span>The Foundation</span>
+			<div className="p-2 px-10 border border-cyan-800 flex justify-between items-center fixed top-0 container bg-white">
+				<div className="flex items-center">
+					<img src={TFLogo} width={50} height={50} />
+					<span>The Foundation</span>
+				</div>
+				<div>
+					<ul className="flex">
+						<li className="ml-4">lorem</li>
+						<li className="ml-4">lorem</li>
+						<li className="ml-4">lorem</li>
+						<li className="ml-4">lorem</li>
+					</ul>
+				</div>
 			</div>
-            <div>
-                <ul className="flex">
-                    <li className="ml-4">lorem</li>
-                    <li className="ml-4">lorem</li>
-                    <li className="ml-4">lorem</li>
-                    <li className="ml-4">lorem</li>
-                </ul>
-            </div>
-		</div>
 	);
 };
 
