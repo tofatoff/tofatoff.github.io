29262954428cf9b999a771d10ebd47a28de562b1
tofatoff <mail.mstofa@gmail.com>
2023-10-13 13:41:56 +0700

    add styling to h2

--- src/components/companies/index.jsx
+++ src/components/companies/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Companies = () => {
 	return (
 		<div className="py-10 px-10 border border-cyan-800">
-			<h2>Companies I've internship at</h2>
+			<h2 className="font-semibold text-2xl text-center">Companies I've internship at</h2>
 			<div>
                 
             </div>
