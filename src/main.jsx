7287f34bc2203e5abd65f92fc20ef0586a55d13d
tofatoff <mail.mstofa@gmail.com>
2023-10-13 13:45:32 +0700

    change root

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,6 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementsByTagName('body')[0]).render(
   <RouterProvider router={router} />
 )
